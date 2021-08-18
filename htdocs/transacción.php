1a474b2f28b475c157cfe4f4b773ffe311f3d8ca
Jorge Rodríguez <jorgerodriguez@externosdeloittemx.com>
2021-08-18 11:00:00 -0500

    PDO

--- htdocs/transacción.php
+++ htdocs/transacción.php
@@ -10,30 +10,15 @@ $dbHost = '34.71.209.22';
 $connConfig = [
    PDO::ATTR_TIMEOUT => 5,
    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-  ];
-echo sprintf('mysql:dbname=%s;host=%s', $dbName, $dbHost);
+  ];  
 $dsn = sprintf('mysql:dbname=%s;host=%s', $dbName, $dbHost);
 // Connect to the database
 try {
   $conn = new PDO($dsn, $username, $password, $connConfig);
   if ($conn) {
-    echo "Connected to the $db database successfully!";
+    echo "Connected to the $dbName database successfully!";
   }
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
-
-echo "Connected successfully";
-
-//Retrive Data
-$cantidad = $_POST['cantidad'];
-$pago = $_POST['pago'];
-$ciudad = $_POST['ciudad'];
-$total = $cantidad * 8.56;
-
-$sql = "INSERT INTO transacciones (prod_id, cliente_id, descripcion, cantidad, total, tipo_pago, fecha, ciudad) values
-(1, 1, 'cubrebocas',".$cantidad.",".$total.",'".$pago."','".date('Y-m-d')."', '".$ciudad."')";
-
-$statement = $pdo->prepare($sql);
-$statement->execute();
 ?>
