73dc9d68581779d5bb3b5a5411be9c10bc733d0d
Jorge Rodríguez <jorgerodriguez@externosdeloittemx.com>
2021-08-18 16:02:37 -0500

    ;

--- htdocs/transacción.php
+++ htdocs/transacción.php
@@ -4,7 +4,7 @@ $username = 'root';
 $password = '12345678';
 $dbName = 'dcleaner';
 $dbHost = '34.71.209.22';
-$instance= '/cloudsql/prueba-de-vpc:us-central1:prueba1'
+$instance= '/cloudsql/prueba-de-vpc:us-central1:prueba1';
 $conn = mysqli_connect(null, $username, $password, $dbName, null, $instance);
 if($conn){
   echo Failed;
