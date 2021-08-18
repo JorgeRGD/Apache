bb9fcd53edeabc18911cdcd364b3f95bce55fd88
Jorge Rodríguez <jorgerodriguez@externosdeloittemx.com>
2021-08-18 16:34:11 -0500

    Conexión

--- htdocs/transacción.php
+++ htdocs/transacción.php
@@ -3,12 +3,21 @@
 $username = 'root';
 $password = '12345678';
 $dbName = 'dcleaner';
-$dbHost = '34.71.209.22';
-$instance= '/cloudsql/prueba-de-vpc:us-central1:prueba1';
-$conn = mysqli_connect(null, $username, $password, $dbName, null, $instance);
+$connectionName = 'prueba-de-vpc:us-central1:prueba1';
+$socketDir = '/cloudsql';
+
+// Connect to the database.
+$dsn = sprintf(
+    'mysql:dbname=%s;unix_socket=%s/%s',
+    $dbName,
+    $socketDir,
+    $connectionName
+);
+// Connect to the database.
+$conn = new PDO($dsn, $username, $password, $conn_config);
 if($conn){
-  echo Failed;
+  echo 'Failed';
 } else{
-  echo Ok;
+  echo 'OK';
 }
 ?>
