343fea427ba53880ab2fdab761ed20c8500677cb
Jorge Rodríguez <jorgerodriguez@externosdeloittemx.com>
2021-08-18 10:09:11 -0500

    PDO

--- htdocs/transacción.php
+++ htdocs/transacción.php
@@ -8,41 +8,15 @@ $dbHost = '34.71.209.22';
 
 
 // Connect to the database.
-PDO {
-  try{
-    $connConfig = [
-      PDO::ATTR_TIMEOUT => 5,
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+PDO{
+  $connConfig = [
+        PDO::ATTR_TIMEOUT => 5,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ];
-    $dsn = sprintf('mysql:dbname=%s;host=%s', $dbName, $dbHost);
-    // Connect to the database
-    $conn = new PDO($dsn, $username, $password, $connConfig);
-  } catch (TypeError $e) {
-          throw new RuntimeException(
-                sprintf(
-                    'Invalid or missing configuration! Make sure you have set ' .
-                    '$username, $password, $dbName, and $dbHost (for TCP mode) ' .
-                    'or $connectionName (for UNIX socket mode). ' .
-                    'The PHP error was %s',
-                    $e->getMessage()
-                ),
-                $e->getCode(),
-                $e;
-            );
-        } catch (PDOException $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Could not connect to the Cloud SQL Database. Check that ' .
-                    'your username and password are correct, that the Cloud SQL ' .
-                    'proxy is running, and that the database exists and is ready ' .
-                    'for use. For more assistance, refer to %s. The PDO error was %s',
-                    'https://cloud.google.com/sql/docs/mysql/connect-external-app',
-                    $e->getMessage()
-                ),
-                $e->getCode(),
-                $e
-            );
-        }
+  $dsn = sprintf('mysql:dbname=%s;host=%s', $dbName, $dbHost);
+// Connect to the database
+  $conn = new PDO($dsn, $username, $password, $connConfig);
+}
 // Check connection
 if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
