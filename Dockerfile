9ec8dd82f22f8381e535b411fcae98be40594247
Jorge <jorgerodriguez@externosdeloittemx.com>
2021-08-18 00:06:52 +0000

    Prueba Apache

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM php:8.0-apache
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+WORKDIR /var/www/html
+COPY /htdocs/DBInitializer.php /htdocs/compra.php  /htdocs/index.html /htdocs/transacción.php  /var/www/html/
+COPY /htdocs/css /var/www/html/css
+COPY /htdocs/flexslider /var/www/html/flexslider
+COPY /htdocs/font /var/www/html/font
+COPY /htdocs/images /var/www/html/images
+COPY /htdocs/js /var/www/html/js
+EXPOSE 80
+
+RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf &&\
+    a2enmod rewrite &&\
+    a2dissite 000-default &&\
+    service apache2 restart
---
+++ htdocs/transacción.php
@@ -0,0 +1,40 @@
+<?php
+// Config conection
+require __DIR__ . 'DBInitializer.php';
+$username = 'root';
+$password = '12345678';
+$dbName = 'dcleaner';
+$connectionName = getenv("prueba-de-vpc:us-central1:prueba");
+$socketDir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
+
+
+// Connect to the database.
+$connConfig = [
+        PDO::ATTR_TIMEOUT => 5,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
+$conn =  initTcpDatabaseConnection(
+        $username,
+        $password,
+        $dbName,
+        $dbHost,
+        $connConfig);
+// Check connection
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+
+//Retrive Data
+$cantidad = $_POST['cantidad'];
+$pago = $_POST['pago'];
+$ciudad = $_POST['ciudad'];
+$total = $cantidad * 8.56;
+
+$sql = "INSERT INTO transacciones (prod_id, cliente_id, descripcion, cantidad, total, tipo_pago, fecha, ciudad) values
+(1, 1, 'cubrebocas',".$cantidad.",".$total.",'".$pago."','".date('Y-m-d')."', '".$ciudad."')";
+
+$statement = $pdo->prepare($sql);
+$statement->execute();
+?>
