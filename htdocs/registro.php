e51d879b24557ff8c48ded9b9b57e0493607be54
Jorge Rodríguez <jorgerodriguez@externosdeloittemx.com>
2021-08-19 12:07:09 -0500

    Registro

--- htdocs/registro.php
+++ htdocs/registro.php
@@ -23,7 +23,7 @@ try {
 }
 
 $email = 'flora@gmail.com';
-$sql = 'SELECT * FROM clientes WHERE email ='.$email;
+$sql = "SELECT * FROM clientes WHERE email '".$email."'";
 $statement = $conn->prepare($sql);
 $statement->execute();
 $user = $statement->fetchAll(PDO::FETCH_ASSOC);
