2367d8edc663eead9c3c90ffea98800545bc9bfa
JorgeRGD <88907413+JorgeRGD@users.noreply.github.com>
2021-08-19 11:38:08 -0500

    Update transacción.php

--- htdocs/transacción.php
+++ htdocs/transacción.php
@@ -3,25 +3,32 @@
 $username = 'root';
 $password = '12345678';
 $dbName = 'dcleaner';
-$connectionName = 'prueba-de-vpc:us-central1:prueba1';
-$socketDir = '34.71.209.22/cloudsql';
+$dbHost = '34.134.50.99';
 
-$conn_config = [
+
+// Connect to the database.
+$connConfig = [
    PDO::ATTR_TIMEOUT => 5,
    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-  ];
-// Connect to the database.
-$dsn = sprintf(
-    'mysql:dbname=%s;unix_socket=%s/%s',
-    $dbName,
-    $socketDir,
-    $connectionName
-);
-// Connect to the database.
-$conn = new PDO($dsn, $username, $password, $conn_config);
-if($conn){
-  echo 'Failed';
-} else{
-  echo 'OK';
+  ];  
+$dsn = sprintf('mysql:dbname=%s;host=%s', $dbName, $dbHost);
+// Connect to the database
+try {
+  $conn = new PDO($dsn, $username, $password, $connConfig);
+  if ($conn) {
+    echo "Connected to the $dbName database successfully!";
+  }
+} catch (PDOException $e) {
+	echo $e->getMessage();
 }
+
+$cantidad = $_POST['cantidad'];
+$pago = $_POST['pago'];
+$ciudad = $_POST['ciudad'];
+$total = $cantidad * 8.56;
+$sql = "INSERT INTO transacciones (prod_id, cliente_id, descripcion, cantidad, total, tipo_pago, fecha, ciudad) values
+(1, 1, 'cubrebocas',".$cantidad.",".$total.",'".$pago."','".date('Y-m-d')."', '".$ciudad."')";
+$statement = $conn->prepare($sql);
+$statement->execute(); 
+
 ?>
